e9547d8b606f6edc008c3daa421a4cc3771319ee
Jeremy <deveneyjeremy0@gmail.com>
2025-11-18 11:27:12 +0100

    Fix Get hamsters by Id pour récuperer que celui du user

--- src/Controller/HamstersController.php
+++ src/Controller/HamstersController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Hamsters;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 
 final class HamstersController extends AbstractController
@@ -32,6 +33,25 @@ final class HamstersController extends AbstractController
     #[Route('/api/hamsters/{id}', name: 'hamsters_by_id', methods: ['GET'])]
     public function getHamstersById(Hamsters $hamsters): JsonResponse
     {
+        $user = $this->getUser();
+
+        if (!$user instanceof User) {
+            return $this->json(
+                ['message' => 'Utilisateur non authentifié.'],
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+
+        $isAdmin = $this->isGranted('ROLE_ADMIN');
+        $isOwner = $hamsters->getOwner()->getId() === $user->getId();
+
+        if (!$isAdmin && !$isOwner) {
+            return $this->json(
+                ['message' => 'Accès refusé.'],
+                Response::HTTP_FORBIDDEN
+            );
+        }
+
         return $this->json([
             'hamsters' => $hamsters,
         ], Response::HTTP_OK, [], ['groups' => ['hamster_list']]);
